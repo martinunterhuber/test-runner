38992364721969a4c6e09085fc23e91cca742513
Martin Unterhuber <m.unterhuber99@gmail.com>
2021-11-23 12:15:32 +0100

    Added metrcis

--- src/main/java/at/unterhuber/test_runner/Runner.java
+++ src/main/java/at/unterhuber/test_runner/Runner.java
@@ -1,5 +1,8 @@
 package at.unterhuber.test_runner;
 
+import com.github.mauricioaniche.ck.CK;
+import com.github.mauricioaniche.ck.CKClassResult;
+import com.github.mauricioaniche.ck.CKNotifier;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.LauncherSession;
@@ -14,12 +17,39 @@ import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.*;
 
 public class Runner {
+    public static Map<String, CKClassResult> getMetrics(String path) {
+        Map<String, CKClassResult> results = new HashMap<>();
+
+        new CK().calculate(path + "src/main/", new CKNotifier() {
+            @Override
+            public void notify(CKClassResult result) {
+                results.put(result.getClassName(), result);
+            }
+
+            @Override
+            public void notifyError(String sourceFilePath, Exception e) {
+                System.err.println("Error in " + sourceFilePath);
+                e.printStackTrace(System.err);
+            }
+        });
+
+        return results;
+    }
+
     public static void main(String[] args) throws MalformedURLException {
+        Map<String, CKClassResult> metrics = getMetrics(args[0]);
+
+        for(Map.Entry<String, CKClassResult> entry : metrics.entrySet()){
+            System.out.println(entry.getValue().getClassName() + ": " + entry.getValue().getNumberOfMethods());
+        }
+
         File file = new File(args[0] + "/build/classes/java/test");
         URL[] urls = new URL[]{file.toURI().toURL()};
         ClassLoader cl = new URLClassLoader(urls);
@@ -44,31 +74,5 @@ public class Runner {
         TestExecutionSummary summary = listener.getSummary();
         summary.printTo(new PrintWriter(System.out));
         summary.printFailuresTo(new PrintWriter(System.out));
-
-
-//        Map<String, CKClassResult> results = new HashMap<>();
-//
-//        new CK().calculate("/home/martin/Webcrawler-CleanCode", new CKNotifier() {
-//            @Override
-//            public void notify(CKClassResult result) {
-//
-//                // Store the metrics values from each component of the project in a HashMap
-//                results.put(result.getClassName(), result);
-//
-//            }
-//
-//            @Override
-//            public void notifyError(String sourceFilePath, Exception e) {
-//                System.err.println("Error in " + sourceFilePath);
-//                e.printStackTrace(System.err);
-//            }
-//        });
-//
-//        // Write the metrics value of each component in the csv files
-//        for(Map.Entry<String, CKClassResult> entry : results.entrySet()){
-//            System.out.println(entry.getValue().getClassName() + ": " + entry.getValue().getNumberOfMethods());
-//        }
-//
-//        System.out.println("Metrics extracted!!!");
     }
 }
