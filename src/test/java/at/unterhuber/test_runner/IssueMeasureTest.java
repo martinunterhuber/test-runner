265a610c7c781d74f30bc95d3683f084a0224a8a
Martin Unterhuber <m.unterhuber99@gmail.com>
2022-01-22 11:20:01 +0100

    Added tests for bugmeasure and issuemeasure

---
+++ src/test/java/at/unterhuber/test_runner/IssueMeasureTest.java
@@ -0,0 +1,33 @@
+package at.unterhuber.test_runner;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class IssueMeasureTest {
+    private IssueMeasure issueMeasure;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        issueMeasure = new IssueMeasure(new GradlePathHandler(System.getProperty("user.dir") + "/testProject"));
+        issueMeasure.findIssues();
+    }
+
+    @Test
+    public void test_getIssues() {
+        Assertions.assertEquals(1, issueMeasure.getIssues().size());
+    }
+
+    @Test
+    public void test_getTestIssues() {
+        Assertions.assertEquals(3, issueMeasure.getTestIssues().size());
+    }
+
+    @AfterEach
+    public void cleanup() {
+        issueMeasure = null;
+    }
+}
