b2d593f5b41108a0c02cb7a8b64a506879da9739
Martin Unterhuber <m.unterhuber99@gmail.com>
2022-01-22 11:53:48 +0100

    Added dependency resolver Test

---
+++ src/test/java/at/unterhuber/test_runner/DependencyResolverTest.java
@@ -0,0 +1,41 @@
+package at.unterhuber.test_runner;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class DependencyResolverTest {
+    private final String PACKAGE = "at.unterhuber.test";
+    private DependencyResolver resolver;
+
+    @BeforeEach
+    public void setup() throws IOException, ClassNotFoundException {
+        ProjectPathHandler handler = new GradlePathHandler(System.getProperty("user.dir") + "/testProject");
+        FileClassLoader cl = new FileClassLoader(handler);
+        cl.initClassLoader();
+        cl.loadTestClasses();
+        resolver = new DependencyResolver(cl, handler, PACKAGE);
+    }
+
+    @Test
+    public void test_resolveDependencies() throws IOException {
+        Set<String> expected = Set.of("at.unterhuber.test.ClassToTest", "at.unterhuber.test.subpackage.AnotherClassToTest", "at.unterhuber.test.subpackage.Test3", "at.unterhuber.test.Junit4Test1");
+        Assertions.assertEquals(expected, resolver.resolveDependenciesFor(List.of("at.unterhuber.test.ClassToTest")));
+    }
+
+    @Test
+    public void test_resolveDependenciesRecursive() throws IOException {
+        Set<String> expected = Set.of("at.unterhuber.test.ClassToTest", "at.unterhuber.test.subpackage.AnotherClassToTest", "at.unterhuber.test.subpackage.Test3", "at.unterhuber.test.Junit4Test1");
+        Assertions.assertEquals(expected, resolver.resolveDependenciesFor(List.of("at.unterhuber.test.ClassToTest"), 5));
+    }
+
+    @AfterEach
+    public void cleanup() {
+        resolver = null;
+    }
+}
